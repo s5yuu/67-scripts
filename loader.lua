32ae17c0754d8510308e2f22945e8826a7f544a4
s5yuu <frcasendev@gmail.com>
2026-04-18 23:23:28 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -10,7 +10,8 @@ task.spawn(function()
         [82866880824588] = "https://api.luarmor.net/files/v4/loaders/ac69cb6855ac9e9c1f3711ad140d5818.lua",
         [81310542478972] = "https://api.luarmor.net/files/v4/loaders/ac69cb6855ac9e9c1f3711ad140d5818.lua",
         [71832465156084] = "https://api.luarmor.net/files/v4/loaders/c27099bd74ba1246c7494d46059b98e7.lua",
-        [14386691987]    = "https://api.luarmor.net/files/v4/loaders/c27099bd74ba1246c7494d46059b98e7.lua"
+        [14386691987] = "https://api.luarmor.net/files/v4/loaders/c27099bd74ba1246c7494d46059b98e7.lua",
+        [14259168147]  = "https://api.luarmor.net/files/v4/loaders/c27099bd74ba1246c7494d46059b98e7.lua"
     }
 
     local url = scripts[PlaceID]
